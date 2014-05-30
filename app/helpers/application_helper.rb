d865229445fc03278e1e25858baa9477f6d4ff17
Bronky <erik.barzdukas@gmail.com>
2014-05-30 09:27:18 -0700

    Added title helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     # Returns the full title on a per-page basis
     def full_title(page_title)
         base_title = "Rails Tutorial Sample App"
-        if page_title.empy?
+        if page_title.empty?
             base_title
         else
             "#{base_title} | #{page_title}"
