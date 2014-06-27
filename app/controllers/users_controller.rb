a4f6c2bed61676d5afe0f425b69b8e3d3f40cb80
Bronky <erik.barzdukas@gmail.com>
2014-06-26 20:38:11 -0700

    Finished sign in/out functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App!"
         redirect_to @user
-        flash[:success] = "Welcome to the Samle App!"
     else
         render 'new'
     end
